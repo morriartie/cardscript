9b9b671ed4a7d5d7a4a88bec9e544cde3593bebf
morriartie <gabrieainha@gmail.com>
2020-02-11 01:18:34 -0300

    showing field

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Simple Card Game for Terminal
 
 A python card game to run in the terminal through commands
+
+![screenshot](https://i.imgur.com/01HlikC.png)
--- lib.py
+++ lib.py
@@ -4,15 +4,16 @@ class Table():
     def __init__(self, deck1, deck2):
         self.deck1 = deck1
         self.deck2 = deck2
-        #
-        self.deck1_field = []
-        self.deck2_field = []
-        #
-        self.deck1_hp = 10
-        self.deck2_hp = 10
-        self.current_turn = 0
+        self.current_turn = 1
 
-        
+    def show_field(self):
+        deck1_field = ' '.join([str(c) for c in self.deck1.field])
+        deck2_field = ' '.join([str(c) for c in self.deck2.field])
+        print(f"{self.deck1}: {self.deck1.current_hp}/{self.deck1.max_hp} hp\n\n")
+        print(f"FIELD: {deck1_field}")
+        print(f"-------------------------------(turn:{self.current_turn})")
+        print(f"FIELD: {deck2_field}")
+        print(f"\n\n{self.deck2}: {self.deck2.current_hp}/{self.deck2.max_hp} hp")
 
 
 class Card():
@@ -65,9 +66,15 @@ class Deck():
         self.cards = []
         self.discard = []
         self.hand = Hand(self)
+        self.field = []
+        self.current_hp = 10
+        self.max_hp = 10
 
     def __int__(self):
         return len(self.cards)
+
+    def __str__(self):
+        return self.name
 
     def add_card(self,card):
         self.cards.append(card)
@@ -82,6 +89,15 @@ class Deck():
 
     def get_deck_size(self):
         return len(self.cards)
+
+    def play_card(self, c_index=None):
+        if not c_index:
+            for i,card in enumerate(self.hand.cards):
+                print(f"{i}: {card}")
+            c_index = int(input(">> "))
+        self.field.append(self.hand.cards[c_index])
+        print(f"playing {self.hand.cards[c_index]}")
+        self.hand.cards = [c for i,c in enumerate(self.hand.cards) if i!=c_index]
 
     def search_deck(deckname):
         result = [v for v in Deck.deck_list if v.name.lower()==deckname.lower()]
